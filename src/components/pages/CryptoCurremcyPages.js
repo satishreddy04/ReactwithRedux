66751e94aa1597a688d6f7f0460671644a7f9c6f
satish <sathish04.reddy@gmail.com>
2018-05-16 22:21:30 -0500

    Update CryptoCurremcyPages.js

--- src/components/pages/CryptoCurremcyPages.js
+++ src/components/pages/CryptoCurremcyPages.js
@@ -1,34 +1,16 @@
-body {
-	background-color: white;
-	font-family: $font-family;
-}
+// IMPORT PACKAGE REFERENCES
+import React from 'react';
+import '../../styles/components/_cryptoApp.scss';
 
-.animated {
-	-webkit-animation-duration: 1s;
-	animation-duration: 1s;
-	-webkit-animation-fill-mode: both;
-	animation-fill-mode: both;
-}
+// IMPORT PROJECT REFERENCES
 
-@keyframes fadeIn {
-	0% {
-		opacity: 0;
-	}
-	100% {
-		opacity: 1;
-	}
-}
+//import { ZipCodeBrowser } from '../ZipCodeBrowser/ZipCodeBrowser';
+import { CryptoMainContainer } from '../CryptoCurrecyFiles/CryptoMainContainer';
 
-.fadeIn {
-	-webkit-animation-name: fadeIn;
-	animation-name: fadeIn;
-}
+// COMPONENT
 
-.navbar {
-	border-bottom: 4px solid white;
-	background-color: #21ce99;
-}
-
-.p-3 {
-	background-color: gray;
-}
+export const CryptoCurrencyPage = () => (
+	<div className="p-3">
+		<CryptoMainContainer />
+	</div>
+);
