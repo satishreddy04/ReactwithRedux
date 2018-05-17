a4927a46ef8730e0dfa94c53036bbebf33d7cd7b
satish <sathish04.reddy@gmail.com>
2018-05-16 22:17:10 -0500

    Update CryptoInbox.js

--- src/components/CryptoCurrecyFiles/CryptoInbox.js
+++ src/components/CryptoCurrecyFiles/CryptoInbox.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-
-
+import '../../styles/components/_cryptoApp.scss';
+import { images } from '../../utils/coinIcons';
 class CryptoInbox extends Component {
-    constructor(props, context) {
-        super(props, context);
-    }
+	constructor(props, context) {
+		super(props, context);
+	}
 
-    render() {
-        return (
-            <div>
-                
-                    <h1>crypto inbox</h1>
-                </div>
-           
-        );
-    }
+	render() {
+		var data = this.props.crypto;
+		var listItems = data.map(function(data) {
+			return (
+				<li class="list-group-item list-group-item-action" key={data.id} data-key={data.id}>
+					<div data-key={data.id} className="listDiv">
+						{data.name} <span>({data.symbol})</span>
+					</div>
+				</li>
+			);
+		});
+
+		return (
+			<div class="list-items">
+				<ul className="list-group">{listItems}</ul>
+			</div>
+		);
+	}
 }
 
 export default CryptoInbox;
