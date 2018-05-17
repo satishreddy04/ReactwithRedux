6da280aaf61e1a689dbee7ed2e8a0541c8265d28
satish <sathish04.reddy@gmail.com>
2018-05-16 22:18:49 -0500

    Update CryptoCurremcyPages.js

--- src/components/pages/CryptoCurremcyPages.js
+++ src/components/pages/CryptoCurremcyPages.js
@@ -1,17 +1,16 @@
-
 // IMPORT PACKAGE REFERENCES
 import React from 'react';
+import '../../styles/components/_cryptoApp.scss';
 
 // IMPORT PROJECT REFERENCES
 
-import { ZipCodeBrowser } from '../ZipCodeBrowser/ZipCodeBrowser';
-import {CryptoMainContainer} from '../CryptoCurrecyFiles/CryptoMainContainer';
-
+//import { ZipCodeBrowser } from '../ZipCodeBrowser/ZipCodeBrowser';
+import { CryptoMainContainer } from '../CryptoCurrecyFiles/CryptoMainContainer';
 
 // COMPONENT
 
 export const CryptoCurrencyPage = () => (
-    <div className="p-3">
-       <CryptoMainContainer />
-    </div>
+	<div className="p-3">
+		<CryptoMainContainer />
+	</div>
 );
