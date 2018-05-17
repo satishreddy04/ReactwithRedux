0e63183b4efbfdbdd9f105b1e769664ff0667c68
satish <sathish04.reddy@gmail.com>
2018-05-16 22:17:53 -0500

    Update CryptoMainContainer.js

--- src/components/CryptoCurrecyFiles/CryptoMainContainer.js
+++ src/components/CryptoCurrecyFiles/CryptoMainContainer.js
@@ -1,50 +1,55 @@
 // IMPORT PACKAGE REFERENCES
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
- import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import '../../styles/components/_cryptoApp.scss';
 
 // IMPORT PROJECT REFERENCES
-import  CryptoDetails  from '../CryptoCurrecyFiles/cryptoDetails';
-import  CryptoInbox  from '../CryptoCurrecyFiles/CryptoInbox';
-import {fetchCryptoCurrencyData} from '../../components/state/actions/CryptoAppActions'
+import CryptoDetails from '../CryptoCurrecyFiles/cryptoDetails';
+import CryptoInbox from '../CryptoCurrecyFiles/CryptoInbox';
+import { fetchCryptoCurrencyData } from '../../components/state/actions/CryptoAppActions';
 
 import { fetchZipCodes } from '../state/actions/ZipCodeActions';
-import { LoadingIndicator } from '../shared/LoadingIndicator/LoadingIndicator';
-import { Error } from '../shared/Error/Error';
-
+// import { LoadingIndicator } from '../shared/LoadingIndicator/LoadingIndicator';
+//import { Error } from '../shared/Error/Error';
 
 // COMPONENT
-
+const LoadingIndicator = props => (
+	<div>
+		{props.busy && (
+			<div>
+				<i className="fa fa-spinner fa-spin fa-4x fa-fw" />
+			</div>
+		)}
+	</div>
+);
 class CryptoMainContainer extends Component {
+	constructor(props) {
+		super(props);
+		console.log(this.props.cryptodata.fetched);
+	}
 
-    constructor(props) {
-        super(props);
-    }
+	componentDidMount() {
+		this.props.fetchCryptoCurrencyData();
+	}
 
-    componentDidMount() {
-        this.props.fetchCryptoCurrencyData();
-       
-    }
-
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-sm-3 col-md-2">
-                       <CryptoInbox />
-                    </div>
-                    <div className="col-sm-9 col-md-10">
-                    <CryptoDetails />
-                    </div>
-
-
-                </div>
-
-               
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-sm-5 col-md-4 inbox">
+						<LoadingIndicator busy={this.props.cryptodata.Fetching} />
+						{this.props.cryptodata.fetched && <CryptoInbox crypto={this.props.cryptodata.cryptoData} />}
+					</div>
+					<div className="col-sm-7 col-md-8 inbox-details">
+						<CryptoDetails />
+						{this.props.fetched}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 // CryptoMainContainer.propTypes = {
 //     fetchCryptoCurrencyData: PropTypes.func.isRequired,
@@ -54,13 +59,12 @@ class CryptoMainContainer extends Component {
 //     cryptoData: PropTypes.array.isRequired
 // };
 
-
 CryptoMainContainer.propTypes = {
-    // fetchZipCodes: PropTypes.func.isRequired,
-    fetched: PropTypes.bool.isRequired,
-    fetching: PropTypes.bool.isRequired,
-    failed: PropTypes.bool,
-    cryptoData: PropTypes.array.isRequired
+	// fetchZipCodes: PropTypes.func.isRequired,
+	fetched: PropTypes.bool.isRequired,
+	Fetching: PropTypes.bool.isRequired,
+	failed: PropTypes.bool,
+	cryptoData: PropTypes.array.isRequired
 };
 
 // function mapStateToProps(state){
@@ -70,12 +74,12 @@ CryptoMainContainer.propTypes = {
 // }
 
 // CONFIGURE REACT REDUX
-const mapStateToProps = (state) => {
-    console.log(state.crytpodata)
-    return {
-     
-    };
-}
+const mapStateToProps = state => {
+	console.log(state);
+	return {
+		cryptodata: state.crytpodata
+	};
+};
 
 // const mapStateToProps = state => {
 //      const { Fetching, fetched, failed, cryptoData } = state.cryptodata;
@@ -83,12 +87,9 @@ const mapStateToProps = (state) => {
 //      return { Fetching, fetched, failed, cryptoData };
 // };
 
-const mapDispatchToProps = dispatch => (
-    bindActionCreators({ fetchCryptoCurrencyData }, dispatch)
-);
+const mapDispatchToProps = dispatch => bindActionCreators({ fetchCryptoCurrencyData }, dispatch);
 
- const hoc = connect(mapStateToProps, mapDispatchToProps)(CryptoMainContainer);
-
+const hoc = connect(mapStateToProps, mapDispatchToProps)(CryptoMainContainer);
 
 // EXPORT COMPONENT
 export { hoc as CryptoMainContainer };
